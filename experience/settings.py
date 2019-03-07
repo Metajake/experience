621e6b6ab0f80b87a30c96a27dd884721bee1bbf
metajake <jaynoco@gmail.com>
2019-03-07 17:33:09 +0000

    Updated Settings for Webfaction.

--- experience/settings.py
+++ experience/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^=v_(6z&9isxj%c&k*na4utpx8pqwh_c))!&wzo5)g_2ga00rz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'experience.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'experience',
+	'USER': 'jacob',
+	'PASSWORD': 'dbPass18!',
+	'HOST': '127.0.0.1',
+	'PORT': '5432',
     }
 }
 
