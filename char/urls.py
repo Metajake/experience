fe04062f449643dfd3509b23e0e0e071eb991288
metajake <jaynoco@gmail.com>
2019-03-06 22:26:52 -0600

    Can't Add Experience unless it's been 15 minutes.

--- char/urls.py
+++ char/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('add', views.addExp, name="addExp"),
+    path('<str:notification>', views.home, name="notification"),
 ]
--- char/views.py
+++ char/views.py
@@ -8,37 +8,42 @@ from .models import Experience, Character
 from .forms import ExperienceAddForm
 
 @login_required
-def home(request):
+def home(request, notification=""):
     char = Character.objects.get(user=request.user)
     exps = Experience.objects.filter(character=request.user.character)
     context = {
         'char': char,
         'exps': exps,
     }
+    if len(notification) > 0:
+        context['notification'] = notification
     return render(request, 'char/home.html', context)
 
 @login_required
 def addExp(request):
+    context = {}
     if request.method == "POST":
-        expAddForm = ExperienceAddForm(request.POST)
-        if expAddForm.is_valid():
-            newExp = expAddForm.save(commit=False)
-            newExp.experienceDate = timezone.now()
-            newExp.character = request.user.character
-            # print(request.user.character['STR'])
-            print(type(request.POST.get('type')))
-            # request.user.character[request.POST.get('type')] += 1
-            currentStat = getattr(request.user.character, request.POST.get('type'))
-            setattr(request.user.character, request.POST.get('type'), currentStat+1)
-            request.user.character.save()
-            newExp.save()
-            return redirect('/')
+        c = Character.objects.get(user=request.user)
+        if timezone.now() >= c.lastTimeExperienced + timezone.timedelta(minutes=15):
+            expAddForm = ExperienceAddForm(request.POST)
+            if expAddForm.is_valid():
+                newExp = expAddForm.save(commit=False)
+                newExp.experienceDate = timezone.now()
+                newExp.character = request.user.character
+                currentStat = getattr(request.user.character, request.POST.get('type'))
+                setattr(request.user.character, request.POST.get('type'), currentStat+1)
+                setattr(request.user.character, 'lastTimeExperienced', timezone.now())
+                request.user.character.save()
+                newExp.save()
+                return redirect('/')
+            else:
+                return redirect('/')
         else:
-            return redirect('/')
+            context['notification'] = "Hasn't been 15 minutes."
+            context['char'] = Character.objects.get(user=request.user)
+            return redirect('Hasn\'t been 15 minutes.', context)
     else:
         expAddForm = ExperienceAddForm()
-        context = {
-            'expAddForm' : expAddForm,
-        }
+        context['expAddForm'] = expAddForm
     context['welcomeMessage'] = "Add an Experience"
     return render(request, 'char/add.html', context)
